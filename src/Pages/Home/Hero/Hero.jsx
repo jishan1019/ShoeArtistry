1546bc1d3c89513f364a01866256c324f0627529
jishan1019 <imranislamjishan200@gmail.com>
2023-10-15 01:17:57 +0600

    right side image design complite

--- src/Pages/Home/Hero/Hero.jsx
+++ src/Pages/Home/Hero/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <section className="bg-white min-h-[25vh] md:min-h-[65vh] flex md:justify-between items-center">
       {/* Shoee Details */}
-      <div className="w-1/2 h-[25vh] md:h-[60vh] pl-4 md:pl-9 ">
+      <div className="w-1/2 h-[26vh] md:h-[60vh] pl-4 md:pl-12 ">
         <div className="mt-4 md:-mt-2">
           <img className="w-10 md:w-20" src={nike} alt="" />
         </div>
@@ -30,7 +30,7 @@ const Hero = () => {
           </button>
         </div>
 
-        <div className="flex justify-start gap-2 items-center text-sm mt-8">
+        <div className="md:flex justify-start gap-2 items-center text-sm mt-8 hidden">
           <div className="text-center p-2 shadow-lg rounded-md ">
             <h2 className="font-bold text_primary text-lg">20K</h2>
             <p className="font-semibold">Orders Complite</p>
@@ -47,7 +47,39 @@ const Hero = () => {
       </div>
 
       {/* Shoee Image */}
-      <div className="w-1/2 h-[25vh] md:h-[60vh] pl-4 md:pl-9"></div>
+      <div className="w-1/2 h-[25vh] md:h-[60vh] relative pr-0 md:pr-16">
+        <div>
+          <div className="w-[60%] rounded-b-lg h-[6vh] mt-1 md:h-[16vh] shadow-lg mx-auto primary_bg  overflow-hidden"></div>
+
+          <div className="w-[60%] rounded-t-lg mb-1 h-[17vh] md:h-[41vh] shadow-lg mt-3 md:mt-8 mx-auto bg-gradient-to-b from-cyan-500 to-[#037abe] relative overflow-hidden grid place-items-center">
+            <p
+              className="text-4xl md:text-9xl absolute font-bold"
+              style={{
+                WebkitTextStroke: "2px white",
+                color: "transparent",
+              }}
+            >
+              NIKE
+            </p>
+          </div>
+        </div>
+
+        <div className="absolute top-0 right-1 flex flex-col items-center justify-center h-full md:mr-4">
+          <p className="rounded-full p-2 bg-red-400"></p>
+          <p className="rounded-full p-2 mt-2 bg-blue-400"></p>
+          <p className="rounded-full p-2 mt-2 bg-yellow-500"></p>
+          <p className="rounded-full p-2 mt-2 bg-blue-700"></p>
+          <p className="rounded-full p-2 mt-2 bg-black"></p>
+        </div>
+
+        <div className="absolute top-8 pr-4 md:pr-16 md:ml-16 ">
+          <img
+            className="md:h-[56vh]"
+            src="https://demo.joomlabuff.com/shoeland/images/joomlabuff/intro-red-shoe.png"
+            alt=""
+          />
+        </div>
+      </div>
     </section>
   );
 };
