1f5c75154f41efb0a4ecdac6676f18c26709891b
jishan1019 <imranislamjishan200@gmail.com>
2023-10-15 13:06:28 +0600

    best product all design complite

--- src/Pages/Home/BestProduct/BestProduct.jsx
+++ src/Pages/Home/BestProduct/BestProduct.jsx
@@ -1,14 +1,11 @@
-import React from "react";
-import SectionTitle from "../../Share/SectionTitle/SectionTitle";
-import icon from "../../../assets/fabicon.png";
-import CircelBg from "../../Share/CircelBg/CircelBg";
+import { BsArrowRight } from "react-icons/bs";
 
 const BestProduct = () => {
   return (
     <section className="bg-white md:-mt-8 flex justify-between items-center flex-col md:flex-row md:pr-12 px-4 py-8">
       <div className="w-full md:w-1/2 relative">
         <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">
-          <div className="bg-[#5FB9EB] h-56 w-56 md:h-80 md:w-80 p-6 rounded-full  overflow-hidden ">
+          <div className="bg-[#5FB9EB] shadow-2xl h-56 w-56 md:h-80 md:w-80 p-6 rounded-full  overflow-hidden ">
             <div className=" rounded-full h-full bg-[#73C2ED] "></div>
           </div>
         </div>
@@ -22,24 +19,32 @@ const BestProduct = () => {
         </div>
       </div>
 
-      <div className="w-full md:w-1/2 mx-auto">
-        <h1>New Arrival Running shoes for the men and women</h1>
+      <div className="w-full md:w-1/2 mx-auto space-y-5">
+        <h1 className="font-bold text-2xl md:text-4xl">
+          New Arrival Running shoes for the men and women
+        </h1>
 
-        <p>
+        <p className="text-xs md:text-[16px] leading-6">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum
           aspernatur harum omnis pariatur nostrum ipsam tenetur sequi reiciendis
           nesciunt corporis est laboriosam asperiores iste quasi maxime delectus
           alias, consequuntur itaque.
         </p>
 
-        <p>
+        <p className="text-xs md:text-[16px] leading-6">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit accusamus
           iste reprehenderit fugit hic iusto, fuga quis maiores velit incidunt
           non consequatur, quisquam consequuntur sed libero minima doloribus
           possimus nihil.
         </p>
 
-        <h1>Read More </h1>
+        <div className="flex items-center justify-start font-bold gap-2">
+          <h1 className="text_primary cursor-pointer">Read More </h1>
+
+          <p className="text_primary mt-1">
+            <BsArrowRight />
+          </p>
+        </div>
       </div>
     </section>
   );
